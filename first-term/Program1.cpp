03324896696aeb1337a448d6e5d49f7813f07a73
Santiago Pérez <82344127+Pasta43@users.noreply.github.com>
2022-02-01 09:57:37 -0500

    Create Program1.cpp

---
+++ first-term/Program1.cpp
@@ -0,0 +1,28 @@
+/*
+* Date: 2022-02-01
+* Author: Santiago Pérez González
+* Course: Paralell and Distributed Computing
+* Topic: first application to measure the performance
+*/
+
+/*Defining the interfaces*/
+#include<stdio.h>
+/*Const that defines the matrix size*/
+#define size 32768
+int matrix[size][size];
+
+/*Main function
+
+This executes the course name, the author's name and what will do the program
+*/
+int main(void) {
+	printf("Computación paralela y distribuida\n");
+	printf("Santiago Pérez González\n");
+	printf("\nPrograma que llena una matriz de tamaño %d",size);
+	for(int i = 0; i<size; i++) {
+		for(int j = 0; j<size; j++) {
+			matrix[i][j] = 47;
+		}
+	}
+	return 0;
+}
