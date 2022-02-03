1e44da7d9baaad124cc4aa14a511192a83e96a33
Santiago Pérez <santiago.perez01@correo.usa.edu.co>
2022-02-03 15:15:36 +0000

    Added the print of 3 matrixes

--- first-term/MyProgram3.cpp
+++ first-term/MyProgram3.cpp
@@ -18,23 +18,45 @@
 
 using namespace std;
 /**
- Function that prints in console a matrix
+ * Function that prints in console a matrix
+ * @param M that its the matrix will be printed
  */
 
 void printMatrix(vector<vector<int>> M){
 	for(int i=0;i<M.size();i++){
 		for(int j=0;j<M.size();j++){
-			cout<<M[i][j]<<"\t";
+			cout<<M[i][j]<<" ";
 		}
 		cout<<endl;
 	}
 
 }
+
+/**
+* Main function
+* @param argc that is the number of arguments
+* @param argv that is a vector with the arguments
+*/
 int main (int argc, char** argv){
 	int i,j,k;
 	int N=atoi(argv[1]);
 	/*It is used the vector class to create the matrixes*/
 	cout<< "Número ingresado: "<<N<<endl;
-	vector<vector<int>> vector1(N,vector<int>(N,0));
-	printMatrix(vector1);	
+	vector<vector<int>> M1(N,vector<int>(N,0));
+	vector<vector<int>> M2(N,vector<int>(N,0));
+	vector<vector<int>> MR(N,vector<int>(N,0));
+	//Se imprime M1
+	cout<<"Se imprime M1"<<endl;
+	printMatrix(M1);
+	cout<<endl<<endl<<endl;
+	//Se imprime M2
+	cout<<"Se imprime M2"<<endl;
+	printMatrix(M2);
+	cout<<endl<<endl<<endl;
+	//Se imprime MR
+	cout<<"Se imprime MR"<<endl;
+	cout<<endl<<endl<<endl;
+	printMatrix(MR);
+	
+		
 }
