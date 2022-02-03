f19599ec2ea3b345e3e3dfe4b043650243dce300
Santiago Pérez <santiago.perez01@correo.usa.edu.co>
2022-02-03 14:36:59 +0000

    Added MyProgram3.cpp

---
+++ first-term/MyProgram3.cpp
@@ -0,0 +1,20 @@
+/*
+* Date: 2022-02-01
+* Author: Santiago Pérez González
+* Course: Paralell and Distributed Computing
+* Topic: build of first benchmark
+* Benchmark: toy app thath allows to meausre performance 
+* of a specific aspet of the computer
+*/
+
+/* Its required to impolement an app that multiply 2 matrixes
+   the algorithm to use will be the clasic (rows * columns)
+*/
+
+#include <iostream>
+using namespace std;
+int main (int argc, char** argv){
+	int i,j,k;
+	int N=atoi(argv[1]);
+	cout<< "Número ingresado: "<<N<<endl;
+}
