ac5245dc89bfbd054af9a746fd3704e9ee3250b1
Santiago Pérez <santiago.perez01@correo.usa.edu.co>
2022-02-03 15:01:52 +0000

    Added printMatrix function

--- first-term/MyProgram3.cpp
+++ first-term/MyProgram3.cpp
@@ -5,6 +5,8 @@
 * Topic: build of first benchmark
 * Benchmark: toy app thath allows to meausre performance 
 * of a specific aspet of the computer
+* 
+* to compile: g++ -g MyProgram3.cpp -o MiPrograma3
 */
 
 /* Its required to impolement an app that multiply 2 matrixes
@@ -12,9 +14,27 @@
 */
 
 #include <iostream>
+#include <vector>
+
 using namespace std;
+/**
+ Function that prints in console a matrix
+ */
+
+void printMatrix(vector<vector<int>> M){
+	for(int i=0;i<M.size();i++){
+		for(int j=0;j<M.size();j++){
+			cout<<M[i][j]<<"\t";
+		}
+		cout<<endl;
+	}
+
+}
 int main (int argc, char** argv){
 	int i,j,k;
 	int N=atoi(argv[1]);
+	/*It is used the vector class to create the matrixes*/
 	cout<< "Número ingresado: "<<N<<endl;
+	vector<vector<int>> vector1(N,vector<int>(N,0));
+	printMatrix(vector1);	
 }
