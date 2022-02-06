ff003c1139b1bdb5266f0f07146069af8044995f
Santiago Pérez <82344127+Pasta43@users.noreply.github.com>
2022-02-06 20:53:25 +0000

    Added presentation v2

--- first-term/workshop-1/presentation.cpp
+++ first-term/workshop-1/presentation.cpp
@@ -1,12 +1,7 @@
 #include <stdio.h>  
-#include <windows.h>  
-void gotoxy(int x,int y){  
-    HANDLE hcon;  
-    hcon = GetStdHandle(STD_OUTPUT_HANDLE);  
-    COORD dwPos;  
-    dwPos.X = x;  
-    dwPos.Y= y;  
-    SetConsoleCursorPosition(hcon,dwPos);  
+
+void gotoxy(int x,int y){
+	printf("%c[%d;%df",0x1B,y,x);
 }
 
 void presentation(){
