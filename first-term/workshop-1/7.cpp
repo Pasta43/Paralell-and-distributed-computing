8b63c5842ca355b6720bd8b94b98e580d1cc9f13
Santiago Pérez <santiago.perez01@correo.usa.edu.co>
2022-02-06 11:06:31 -0500

    Fixed some bugs with the exercise

--- first-term/workshop-1/7.cpp
+++ first-term/workshop-1/7.cpp
@@ -1,5 +1,5 @@
 /*
-* Date: 2022-02-03 - format: YYYY-mm-dd
+* Date: 2022-02-06 - format: YYYY-mm-dd
 * Author: Santiago Pérez González
 * Course: Paralell and Distributed Computing
 * Topic: Seventh point of the workshop 1. 
@@ -19,7 +19,7 @@ using namespace std;
 */
 double calculateSum(int b,double x,double y){
 	double sum=0;
-	for(int a=0;a<b;a++){
+	for(int a=0;a<=b;a++){
 		sum+=1/(x+a*y);
 	}
 	return sum;
@@ -27,19 +27,24 @@ double calculateSum(int b,double x,double y){
 
 int main(int argv,char** argc){
 	int b;
-	double x=0,y,sum=0;
+	double x=0,y=0,sum=0;
 	cout<<"Bienvenido"<<endl<<"Este es un programa que calcula la sumatoria sum_{a=0}^{b}{frac{1}{x+ay}}"<<endl;
 	cout<<"Digite el valor del límite de la sumatoria (b): ";
 	cin>>b;
-	while(x==0){
+	while(x<=0){
 		cout<<"Digite el valor del parámetro x: ";
 		cin>>x;
-		if(x==0){
-			cout<<"El valor del parámetro x no puede ser cero"<<endl;
+		if(x<0){
+			cout<<"El valor del parámetro x debe ser positivo"<<endl;
 		}
 	}
-	cout<<"Digite el valor del parámetro y: ";
-	cin>>y;
+	while(y<=0){
+                cout<<"Digite el valor del parámetro y: ";
+                cin>>y;
+                if(y<0){
+                        cout<<"El valor del parámetro y debe ser positivo"<<endl;
+                }
+        }
 	sum=calculateSum(b,x,y);
 	cout<<"El valor de la suma es: "<<sum<<" "<<endl;
 
