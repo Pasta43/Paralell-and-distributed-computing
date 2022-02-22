16419ac5bf848a8ed7792c683656219fdeb15880
Santiago Pérez <82344127+Pasta43@users.noreply.github.com>
2022-02-22 15:24:10 +0000

    Added function that multiplicates two matrixes

--- first-term/benchC.cpp
+++ first-term/benchC.cpp
@@ -26,8 +26,11 @@ double ** createMatrix(int size){
 }
 
 /**
-*   
-*
+*   @brief Function that initializes a three matrixes before do a multiplication
+*   @param matA is a pointer that represents a matrix (matrix 1)
+*   @param matB is a pointer that represents a matrix (matrix 2)
+*   @param matC is a pointer that represents a matrix (result matrix when matA*matB)
+*   @param size that is the matrix size of matA,matB,matC
 */
 void initializeMatrixes(int size,double ** &matA, double ** &matB, double ** &matC ){
     int i,j;
@@ -39,9 +42,45 @@ void initializeMatrixes(int size,double ** &matA, double ** &matB, double ** &ma
         }
     }
 }
+/**
+*   @brief Function that destroyes a matrix. This deletes all the rows of a matrix and later puts the matrix pointer as a null pointer 
+*   @param size that is the matrix size N*N
+*   @param matrix is a pointer that represents the matrix 
+* 
+*/
+void destroyMatrix(int size, double** &matrix){
+    if(matrix){
+        int i; 
+        for(i=0;i<size;i++){
+            delete [] matrix[i];
+        }
+        delete [] matrix;
+        matrix=nullptr;
+
+    }
+}
+/**
+*   @brief Function 
+*   @param
+*   @param
+*/
+double ** matrixProduct(int size, double ** &matrix1,double ** &matrix2){
+    int i,j,k;
+    double **matrix = createMatrix(size);
+    for(i=0;i<size;i++){
+        for(j=0;j<size;j++){
+            for(k=0;k<size;k++){
+                matrix[i][j]+=matrix1[i][k]+matrix2[k][j];
+            }   
+        }     
+    }
+    return matrix;
+}
 
 /**
-
+* @brief Function that prints a matrix
+* @param matrix that is the matrix
+* @param size that is the matrix size
 */
 void printMatrix(double **&matrix,int size){
     int i,j;
@@ -65,4 +104,8 @@ int main(int argc,char **argv){
     printMatrix(matA,N);
     printMatrix(matB,N);
     printMatrix(matR,N);
+    matrixProduct(N,matA,matB);
+    destroyMatrix(N,matA);
+    destroyMatrix(N,matB);
+    destroyMatrix(N,matC);
 }
