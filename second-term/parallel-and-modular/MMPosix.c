1025043a08950d46832ccbd8e52dc2c93e86c150
Santiago Perez <santiago.perez01@correo.usa.edu.co>
2022-03-16 19:24:59 -0500

    Added update with beggining of parallel multiplication

---
+++ second-term/parallel-and-modular/MMPosix.c
@@ -0,0 +1,50 @@
+/*Fecha: 2022-03-16
+* Autor: Santiago Pérez
+* Subject: Parallel and Distributed Computing.
+* Topic: Poxis implemetation (Library)
+* Description: Application for matrix multiplication
+  with the classical algorithm (rows x columns) using
+  global variables. Also, using the following functions:
+    1. Creation of double pointer variables for matrices
+    2. Memory reserve for double pointer matrices
+    3. Init matrix
+    4. Function for the matrix's product:
+        4.1.1 The matrix A is divide and those portions 
+        will be sent to an individual thread
+        4.1.2 The matrix's size will be sent in the 
+        execution.
+    5. Matrix's size always squared (NxN)
+    6.  Print matrix with double pointer.
+*/
+
+
+/*Interfaces*/
+#include "module.h"
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <sys/time.h>
+
+/*Global variables*/
+int N, Nthreads;
+double **Ma, **Mb, **Mc;
+
+/*  @breif main(): Main function
+*/
+int main(int argc, char* argv[]){
+    /*Init of global variables*/
+    N           = atof(argv[1]);    /* Matrix's size.*/
+    Nthreads    = atof(argv[2]);    /* Number of threads.*/
+
+    /*Memory creation and reserce for each matrix*/
+    Ma = memReserve(N); 
+    Mb = memReserve(N);
+    Mc = memReserve(N);
+    initMatrix_DoublePointers (Ma, Mb, Mc, N);
+    printMatrix_DoublePointers (Ma, N);
+    printMatrix_DoublePointers (Mb, N);
+    printMatrix_DoublePointers (Mc, N);
+
+    return 0;
+}
