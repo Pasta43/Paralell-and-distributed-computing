eb33c1994035e1123ca5f840b020c954b79def6f
Santiago Pérez <santiago.perez01@correo.usa.edu.co>
2022-04-19 10:29:48 -0500

    Added orbitals test

---
+++ third-term/Cython3/pruebaOrbitas.py
@@ -0,0 +1,38 @@
+import gravedad
+import gravedadCy
+import time
+#Initialized planet for python
+
+earth = gravedad.Planet()
+earth.x=100e3
+earth.y=300e3
+earth.z=300e3
+earth.vx=2e3
+earth.vy=29.87e3
+earth.vz=0.034e3
+earth.m=5.9736e24
+
+time_span=600
+n_steps=4000000
+initTime=time.time()
+gravedad.step_time(planet=earth,time_span=time_span,n_steps=n_steps)
+endTime=time.time()
+totalTime = endTime-initTime
+print("Tiempo en python",totalTime,"s")
+
+earth = gravedadCy.Planet()
+earth.x=100e3
+earth.y=300e3
+earth.z=300e3
+earth.vx=2e3
+earth.vy=29.87e3
+earth.vz=0.034e3
+earth.m=5.9736e24
+initTimeCy=time.time()
+gravedadCy.step_time(planet=earth,time_span=time_span,n_steps=n_steps)
+endTimeCy=time.time()
+totalTimeCy = endTimeCy-initTimeCy
+print("Tiempo en cython",totalTimeCy,"s")
+
+speedUp=totalTime/totalTimeCy
+print("SpeedUp",speedUp)
